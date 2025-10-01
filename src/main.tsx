36502dedf3119e936328ade6b894b9fa3a76afdd
ShaanKapoor10 <shaan2003kapoor@gmail.com>
2025-10-02 01:13:06 +0530

    feat(app): integrated context and screens in App component
    maintained the application in QuizProvider in main.jsx. Updated App.jsx to act as a router, showing the correct screen based on the global game state.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
+import { QuizProvider } from './context/QuizContext.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QuizProvider>
+      <App />
+    </QuizProvider>
+  </React.StrictMode>,
 )
